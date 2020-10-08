6d4376a63fa988ba789aa13b73d22a57708cf3b2
luhnn <luhnn0120@gmail.com>
2020-10-08 21:27:08 +0800

    二叉堆优化

--- Week 02/map-edit.js
+++ Week 02/map-edit.js
@@ -12,8 +12,63 @@ function sleep(t){
   return new Promise(resolve => {
     setTimeout(resolve, t)
   })
+} 
+// 二叉堆
+class Heap{
+  constructor(data, compare){
+    this.data = []
+    this.compare = compare || ( (a,b) => a-b )
+    for (let index = 0; index < data.length; index++) {
+      this.give(data[index])
+    }
+  }
+  get length(){
+    return this.data.length
+  }
+  take(){
+    if(!this.data.length){
+      return
+    }
+    let min = this.data[0]
+    this.data[0] = this.data[this.data.length - 1] 
+    for(let index = 0; index < this.data.length;){
+      if(2*index + 1 > this.data.length - 1) {
+        break
+      }
+      let minChildIndex = 2*index + 1 
+      if(2*index + 2 <= this.data.length - 1 && this.compare(this.data[2*index+1], this.data[2*index+2]) > 0){
+        minChildIndex = 2*index + 2
+      }
+      if(this.compare(this.data[index], this.data[minChildIndex]) > 0){
+        let tmp = this.data[index]
+        this.data[index] = this.data[minChildIndex]
+        this.data[minChildIndex] = tmp
+        index = minChildIndex
+        continue
+      }
+      break
+    }
+    this.data.pop();
+    return min
+  }
+  give(data){
+    this.data.push(data)
+    for(let index = this.data.length - 1; index >=1;) {
+      let parentIndex = Math.floor((index-1)/2)
+      if(this.compare(this.data[parentIndex], this.data[index]) > 0){
+        let tmp = this.data[index]
+        this.data[index] = this.data[parentIndex]
+        this.data[parentIndex] = tmp
+        index = parentIndex
+        continue
+      }
+      break
+    }
+  }
+  show(){
+    console.log(this.data)
+  }
 }
-// TODO: 二叉堆
 class Sorted{
   constructor(data,compare){
     this.data = Object.create(data)
@@ -45,20 +100,24 @@ class Sorted{
 
 // 广度优先寻路算法
 async function findPath(map, start, end){
+  const startTime = new Date().getTime()
   let mapTmp = Object.create(map)
-  let query = new Sorted([start], (a,b) => distance(a) - distance(b))
+  let query = new Heap([start], (a,b) => distance(a) - distance(b))
   while(query.length != 0 ){
     let [x, y] = query.take()
     if(x === end[0] && y === end[1]){
       let path = [end]
       while(x !== start[0] || y !== start[1]){
-        await sleep(10)
+        await sleep(20)
         document.getElementById('container').children[x+ y*100].style.backgroundColor = 'purple'
         let tmp = mapTmp[x + y*100]
         x = tmp[0]
         y = tmp[1]
         path.push(tmp)
       }
+      const endTime = new Date().getTime();
+      console.log('time =====', (endTime - startTime))
+      console.log('step =====', path.length)
       return path
     }
     await insert(x-1, y, [x, y])
@@ -82,7 +141,7 @@ async function findPath(map, start, end){
     if(mapTmp[x + 100*y]){
       return
     }
-    // await sleep(30)
+    // await sleep(20)
     document.getElementById('container').children[x+ y*100].style.backgroundColor = 'lightgreen'
     mapTmp[x + 100*y] = pre
     query.give([x,y])
@@ -128,7 +187,6 @@ document.oncontextmenu = (e)=>{
   e.preventDefault()
 }
 document.getElementsByTagName('button')[0].addEventListener('click', () => {
-  console.log('save')
   localStorage.setItem('map', JSON.stringify(map))
 })
 show()
